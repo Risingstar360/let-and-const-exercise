ac5c05909681f6dd2a26b5cae9badc320d14efb6
Martin <99287130+Risingstar360@users.noreply.github.com>
2022-07-13 10:51:24 +0100

    completed quiz and added to new repository

---
+++ app.js
@@ -0,0 +1,15 @@
+const PI = 3.14;
+
+/*Quiz*/
+//What is the difference between var and let?
+
+//Var is scoped to the function where as let is scoped to the code block
+
+//What is the difference between VAR and const?
+//VAR can be re-declared whereas const cannot.  
+
+//What is the difference between let and const?
+//Both block scoped, let can be re-declared whereas const cannot.  Both can be mutated.
+
+//What is hoisting?
+//Javascript, by default, moves all declarations to the top of the current scope.  This can affect the use of VAR so it is not recommended, where possible to use it.
